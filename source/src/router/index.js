486b88aa2ad66ebbd3dcd2f647b20cbaad5e6b5e
sejinjja <sejinjja@bpnr.co.kr>
2020-08-22 02:50:52 +0900

    기본적인 빌드 코드와, 개발용 코드를 잡아 두었습니다.

---
+++ source/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import HelloWorld2 from '@/components/HelloWorld2'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/HelloWorld',
+      name: 'HelloWorld-dr',
+      component: HelloWorld
+    },
+    {
+      path: '/HelloWorld2',
+      name: 'HelloWorld2',
+      component: HelloWorld2
+    }
+  ]
+})
